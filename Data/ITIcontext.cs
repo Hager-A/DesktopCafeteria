65a83bb61b5aabe946b31b8d859b2acc0db70d91
hager <ha1316625@gmail.com>
2025-06-08 16:12:18 +0300

    add the conection

--- Data/ITIcontext.cs
+++ Data/ITIcontext.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace project.Data
+namespace project
 {
-    class ITIcontext
+    class ITIcontext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-DV1U283\\SQLEXPRESS01;Initial Catalog=DesktopCafeteria;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+            //base.OnConfiguring(optionsBuilder);
+        }
     }
 }
