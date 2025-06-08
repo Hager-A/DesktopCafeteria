cbe95cc3dd6e77eed86eef05999856eebcd607b6
hager <ha1316625@gmail.com>
2025-06-08 16:37:52 +0300

    edit the class product

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -10,8 +10,8 @@ namespace project.Entities
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public int Quant { get; set; }
+        public int Quantity { get; set; }
         public int UnitPrice { get; set; }
-        public string Kind { get; set; }
+        public string Type { get; set; }
     }
 }
