451ca6153cb9ef2e82933c1b8c083f1e3adf7beb
AmiraFathalbab <amira.fathalbab@gmail.com>
2025-06-09 00:15:26 +0300

    create DBManager

---
+++ DAL/DBManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class DBManager
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        DataTable dt;
+        public DBManager() {
+            conn = new SqlConnection("Data Source=DESKTOP-DV1U283\\SQLEXPRESS01;Initial Catalog=DesktopCafeteria;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+
+            //conn = new SqlConnection("Data Source=DESKTOP-N3T3R6C\\SQLEXPRESS;Initial Catalog=DesktopCafeteria;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+            cmd = new SqlCommand("", conn);
+        }
+
+    }
+}
