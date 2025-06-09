ba1c969a3fa11f756f1d5a13a800b66b7d5fb8e7
AmiraFathalbab <amira.fathalbab@gmail.com>
2025-06-09 12:24:13 +0300

    add EmployeeManager

---
+++ Managers/EmployeeManager.cs
@@ -0,0 +1,39 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public static class EmployeeManager
+    {
+        static DBManager db=new DBManager();
+        public static Employee GetEmployee()
+        {
+            DataTable dt = db.GetDataTable("select *from Employees where Email='' and Name=''");
+            Employee res = new Employee();
+            foreach (DataRow dr in dt.Rows) {
+                res = ConvertFromDataRowToEmp(dr);
+                
+            }
+            return res;
+        }
+
+        internal static Employee ConvertFromDataRowToEmp(DataRow dr)
+        {
+            var res = new Employee();
+            int.TryParse(dr[0]?.ToString() ?? "-1", out int x);
+            res.Id = x;
+
+            res.Email = dr[1]?.ToString()?? "ND";
+            res.Name = dr[2]?.ToString() ?? "ND";
+            res.Password = dr[3]?.ToString() ?? "ND";
+            res.Role = dr[4]?.ToString() ?? "ND";
+
+            return res;
+        }
+    }
+}
